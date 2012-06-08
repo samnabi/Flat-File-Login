ede2e6eae4f2335edc4a5e47da3eebf815330afc
Sam Nabi <sam@samnabi.com>
2012-06-08 07:45:35 -0400

    Added current password verification box

--- changepw.php
+++ changepw.php
@@ -2,9 +2,12 @@
 
 <h1>Change password</h1>
 
-<p>Howdy, <strong><?php echo $_SESSION['username']; ?></strong>! Your password is currently <?php echo $_SESSION['password']; ?>. Type your new password below to change it.</p>
+<p>Howdy, <strong><?php echo $_SESSION['username']; ?></strong>! Type your new password below to change it.</p>
 
 <form type="POST" action="writechangepw.php">
+	<label>Current Password:
+		<input name="oldpass" type="password" value="<?php echo $_SESSION['password']; ?>" />
+	</label>
 	<label>New Password:
 		<input name="newpass" type="password" />
 	</label>
